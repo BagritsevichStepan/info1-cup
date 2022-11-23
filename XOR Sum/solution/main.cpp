a886e529c40b3192e5a36e0850cfbc0b0008fded
Stsiapan Bahrytsevich <stepan.bahrytsevich@tum.de>
2022-11-23 12:06:04 +0100

    Change variable names

--- XOR Sum/solution/main.cpp
+++ XOR Sum/solution/main.cpp
@@ -2,25 +2,26 @@
 
 using namespace std;
 
-const int MAX_N = 1e6 + 5;
-pair<int, int> sortArray[2][MAX_N];
-pair<int, int> x[MAX_N];
-int a[MAX_N];
+const int maxN = 1e6 + 5;
 
-bool getBit(const int x, const int k) {
+pair<int, int> sortArray[2][maxN];
+pair<int, int> x[maxN];
+int a[maxN];
+
+bool GetBit(const int x, const int k) {
     return x & (1 << k);
 }
 
-void sort(const int bit, const int n) {
+void Sort(const int bit, const int n) {
     for (int i = 0; i < n; i++) {
-        if (getBit(a[x[i].second], bit)) {
+        if (GetBit(a[x[i].second], bit)) {
             x[i].first |= 1 << bit;
         }
     }
 
     int size[] = {0, 0};
     for (int i = 0; i < n; i++) {
-        int index = getBit(x[i].first, bit);
+        int index = GetBit(x[i].first, bit);
         sortArray[index][size[index]++] = x[i];
     }
 
@@ -32,7 +33,7 @@ void sort(const int bit, const int n) {
     }
 }
 
-void updateP(int& p, const int i, const int value) {
+void UpdateP(int& p, const int i, const int value) {
     p = max(p, i);
     while (i < p && x[i].first + x[p - 1].first >= value) {
         p--;
@@ -53,7 +54,7 @@ int main() {
 
     int result = 0;
     for (int bit = 0; bit < 30; bit++) {
-        sort(bit, n);
+        Sort(bit, n);
 
         int p1 = n;
         int p2 = n;
@@ -61,9 +62,9 @@ int main() {
         const int value = 1 << bit;
 
         for (int i = 0; i < n; i++) {
-            updateP(p1, i, value);
-            updateP(p2, i, 2 * value);
-            updateP(p3, i, 3 * value);
+            UpdateP(p1, i, value);
+            UpdateP(p2, i, 2 * value);
+            UpdateP(p3, i, 3 * value);
             if ((p2 - p1 + n - p3) % 2) {
                 result ^= value;
             }
